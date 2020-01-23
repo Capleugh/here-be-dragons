b896941bbee2433e7d7e64dcd4522fce1a091eb6
Carleigh Crockett <capleugh@gmail.com>
2020-01-23 01:20:06 -0700

    Add result for passing tests in array_of_stories test and all_stories_with_subsection_politics

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -19,7 +19,8 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
-skip
+    result = @hash[:results]
+
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
@@ -27,7 +28,10 @@ skip
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
 
-skip
+    result = @hash[:results].find_all do |story|
+      story[:subsection] == "Politics"
+    end
+
     assert result.is_a? (Array)
     assert_equal 6, result.count
     assert_equal "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight",
